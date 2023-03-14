b4f1b5d5cdc00361dc26ffa7bdda5dfe828e08f6
Amol-Talekar <amol11talekar@gmail.com>
2023-03-14 17:58:06 +0530

    Changed port values

--- app.js
+++ app.js
@@ -17,18 +17,17 @@ const sslCert = {
   cert: fs.readFileSync(path.join(process.cwd(), "keys", "ca.crt")),
 };
 
+app.use("/api/user", router);
 app.use("/", (req, res, next) => {
   res.send("Server is succesfully running in browser...");
 });
-
-app.use("/api/user", router);
-const port = process.env.PORT || 4000;
+const port = 4000 || process.env.PORT;
 
 mongoose
   .connect(
     `mongodb+srv://amol11talekar:${process.env.MONGO_DB_PASSWORD}@cluster0.lt4actf.mongodb.net/LE?retryWrites=true&w=majority`
   )
-  .then(() => app.listen(Number(`${port}`)))
+  .then(() => app.listen(port))
   .then(() => console.log("Database is connected and listening on port 4000"))
   .catch((err) =>
     console.log("there was error in connecting with Database : ", err)
